4bff88053e76f4e0a6422d274ba3a196486a6ff4
igorexec <icheliadinski@gmail.com>
2020-05-15 22:28:17 -0400

    init

---
+++ wallaby.conf.js
@@ -0,0 +1,24 @@
+const packageJson = require('./package.json');
+
+const { env } = packageJson.betterScripts.test;
+const { files: tests } = packageJson.ava;
+
+const envString = Object.entries(env).map(([key, value]) => `${key}=${value}`).join(';')
+
+module.exports = () => ({
+  files: [
+    'src/**/*.js',
+    'data/**/*.*',
+    'tests/__data/**/*.*',
+    'tests/helpers/**/*.js'
+  ],
+  tests,
+  env: {
+    type: 'node',
+    params: {
+      env: envString,
+    }
+  },
+  debug: true,
+  testFramework: 'ava'
+});
